e55c131a37cea82c6e477d5fc2c88ff9abb6c701
RivJd1 <josd_riverao06@unicah.edu>
2026-08-04 01:13:18 -0600

    Sistema Tokens Actualizado

--- routes/api.php
+++ routes/api.php
@@ -13,17 +13,19 @@ Route::prefix('auth')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
     Route::post('/reset-password', [AuthController::class, 'resetPassword']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/me', [AuthController::class, 'me']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me']);
+    });
 });
 
+Route::middleware('auth:sanctum')->group(function () {
 Route::apiResource('aulas', AulaController::class);
 Route::apiResource('secciones', SeccionController::class);
 Route::apiResource('docentes', DocenteController::class);
 Route::apiResource('asignaciones', AsignacionController::class);
-
 Route::post('asignaciones/{asignacion}', [AsignacionController::class, 'update']);
-
 Route::apiResource('periodos-academicos', PeriodoAcademicoController::class);
 Route::apiResource('sesiones-horario', SesionHorarioController::class);
-
+});
