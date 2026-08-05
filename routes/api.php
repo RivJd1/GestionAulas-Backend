95fb07568830ffdc6c1956b19727a2a857739589
471-234 <nayg_melendresc32@unicah.edu>
2026-08-04 18:09:10 -0600

    agregar aulas disponibles

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::prefix('auth')->group(function () {
     });
 });
 
-
+Route::get('aulas/disponibles', [AulaController::class, 'disponibles']);
 Route::apiResource('aulas', AulaController::class);
     Route::apiResource('secciones', SeccionController::class);
     Route::apiResource('docentes', DocenteController::class);
@@ -40,3 +40,4 @@ Route::apiResource('aulas', AulaController::class);
 Route::middleware('auth:sanctum')->group(function () {
 
 });
+ 
